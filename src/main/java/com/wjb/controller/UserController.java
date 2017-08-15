c03d672e9ed40998cc913e3e37655d22a8b62e37
millennium0607 <912436618@qq.com>
2017-08-15 16:16:13 +0800

    update
    删除无效代码

--- src/main/java/com/wjb/controller/UserController.java
+++ src/main/java/com/wjb/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.sun.media.jfxmedia.logging.Logger;
 import com.wjb.db.po.UserT;
 import com.wjb.service.IUserService;
 
